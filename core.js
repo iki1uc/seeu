dbf88c7d2c77407cd9f4043aa54b4cb249428138
iki1uc <entertrainer@web.de>
2026-06-26 18:19:05 +0200

    Update core.js

--- core.js
+++ core.js
@@ -1,1 +1,28 @@
+// SEEÜ: Motor Sync
+
+import { startMotor, stopMotor, onMotorStep } from "../shift/motor.js";
+
+export const SEEUE = {
+  activeStep: null,
+
+  start() {
+    console.log("SEEÜ: Motor wird gestartet…");
+    startMotor();
+  },
+
+  stop() {
+    console.log("SEEÜ: Motor wird gestoppt…");
+    stopMotor();
+  },
+
+  sync() {
+    onMotorStep((step) => {
+      this.activeStep = step;
+      console.log("SEEÜ Sync:", step);
+    });
+  }
+};
+
+// SEEÜ initialisieren
+SEEUE.sync();
 
