5565969777bb50aee133a022579ef1ab27dde770
iki1uc <entertrainer@web.de>
2026-06-26 02:48:40 +0200

    Implement ChipletLoader with register and get methods

--- RA/ra-chiplet-loader.js
+++ RA/ra-chiplet-loader.js
@@ -1,1 +1,15 @@
+// RA – Chiplet Loader
 
+export const ChipletLoader = {
+  chiplets: {},
+
+  // Chiplet registrieren
+  register(name, content) {
+    this.chiplets[name] = content;
+  },
+
+  // Chiplet abrufen
+  get(name) {
+    return this.chiplets[name] || null;
+  }
+};
