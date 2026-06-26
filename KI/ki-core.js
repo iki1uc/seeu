89b9e6cc4ba8073d7e7d151a5bc4ac21d687324d
iki1uc <entertrainer@web.de>
2026-06-26 03:09:08 +0200

    Update ki-core.js

--- KI/ki-core.js
+++ KI/ki-core.js
@@ -1,1 +1,13 @@
+// KI – Quelle für SEEÜ
+export const KI = {
+  data: { message: "SEEÜ: KI bereit." },
+  get() {
+    return this.data;
+  },
+  set(newData) {
+    this.data = newData;
+  }
+};
+
+
 
