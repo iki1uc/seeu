c9243fd132d48666bae72e43590e8eab9054da55
iki1uc <entertrainer@web.de>
2026-06-26 02:47:25 +0200

    Update kompotence.js

--- RA/kompotence.js
+++ RA/kompotence.js
@@ -1,1 +1,22 @@
+// SEEÜ – Kompetenzmodul (KOMPOTENCE)
+
+// Kompetenz = kleinste funktionale Einheit
+export const KOMPOTENCE = {
+  state: {},
+
+  // Kompetenz setzen
+  set(name, value) {
+    this.state[name] = value;
+  },
+
+  // Kompetenz abrufen
+  get(name) {
+    return this.state[name] || null;
+  },
+
+  // Kompetenz prüfen
+  has(name) {
+    return this.state[name] !== undefined;
+  }
+};
 
