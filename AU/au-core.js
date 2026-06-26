b5b2eebc659f1eb544fb4bb6952a62b7d55aba16
iki1uc <entertrainer@web.de>
2026-06-26 03:07:39 +0200

    Update au-core.js

--- AU/au-core.js
+++ AU/au-core.js
@@ -1,1 +1,22 @@
+// AU – Cache für SEEÜ
+// AU speichert nur Daten. Keine Logik, keine KI, kein Routing.
+
+export const AU = {
+  data: {},
+
+  // Daten setzen
+  update(content) {
+    this.data = content;
+  },
+
+  // Daten auslesen
+  read() {
+    return this.data;
+  },
+
+  // AU komplett leeren
+  clear() {
+    this.data = {};
+  }
+};
 
