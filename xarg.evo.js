2fd64670902de343fcfc382507ebbe49ea37ac1c
iki1uc <entertrainer@web.de>
2026-08-09 10:58:59 +0200

    Add EVO object and EVO_MASSE function

--- xarg.evo.js
+++ xarg.evo.js
@@ -1,1 +1,13 @@
+// xarg.evo.js
+export const EVO = {
+  tick: 0,
+  act(seeu){
+    this.tick++;
+    seeu.evoState = "EVO-" + this.tick;
+  }
+};
+
+export function EVO_MASSE(evo){
+  return evo.tick * 2;
+}
 
