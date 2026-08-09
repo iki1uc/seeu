cab7aed1c35895b98f95f06811704b8ae8bfc4a4
iki1uc <entertrainer@web.de>
2026-08-09 09:52:19 +0200

    Add prefetch links for multiple static YAML files

---
+++ PRE.js
@@ -0,0 +1,8 @@
+<link rel="prefetch" href="https://iki1uc.github.io/AQUA/AQUA.static.yml">
+<link rel="prefetch" href="https://iki1uc.github.io/EDIT/EDIT.static.yml">
+<link rel="prefetch" href="https://iki1uc.github.io/CLOU/CLOU.static.yml">
+<link rel="prefetch" href="https://iki1uc.github.io/WETTEN/WETTEN.static.yml">
+
+<link rel="prefetch" href="https://iki1uc.github.io/6iy/6iy.static.yml">
+<link rel="prefetch" href="https://iki1uc.github.io/beu/beu.static.yml">
+<link rel="prefetch" href="https://iki1uc.github.io/xyz/xyz.static.yml">
