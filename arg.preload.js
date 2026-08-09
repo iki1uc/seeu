f9b9f4f37607c8ce22346e4737ddb143ddce33f3
iki1uc <entertrainer@web.de>
2026-08-09 10:25:46 +0200

    Implement ACHS_ANALYSE function
    Added ACHS_ANALYSE function to analyze metrics.

--- arg.preload.js
+++ arg.preload.js
@@ -6,3 +6,11 @@
 <link rel="prefetch" href="https://iki1uc.github.io/6iy/6iy.static.yml">
 <link rel="prefetch" href="https://iki1uc.github.io/beu/beu.static.yml">
 <link rel="prefetch" href="https://iki1uc.github.io/xyz/xyz.static.yml">
+
+  function ACHS_ANALYSE(m){
+  return {
+    dominant: Math.max(m.ort, m.home, m.gate, m.evo, m.radar),
+    mismatch: Math.abs(m.ort - m.home),
+    continuum: m.continuum
+  };
+}
