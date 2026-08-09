9fe1005542bde5b3ebbd652996d4f1d07fbbf3fc
iki1uc <entertrainer@web.de>
2026-08-09 10:28:27 +0200

    Update and rename seeu-test.html to xarg.seeu.js

--- xarg.seeu.js
+++ xarg.seeu.js
@@ -9,3 +9,14 @@
     "SEEÜ Sicht: " + JSON.stringify(AU.read());
 </script>
 
+function GEO_VECTOR_6D(gate, evo){
+  return {
+    x: gate.math.length * 10,
+    y: gate.home.length * 5,
+    z: gate.gate.length * 3,
+    w: evo.tick,
+    v: gate.boost,
+    u: (gate.boost % 9) * 7,
+    sat: gate.gate
+  };
+}
