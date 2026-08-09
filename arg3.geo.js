ce527effffc09888197385061368bb1328136bcd
iki1uc <entertrainer@web.de>
2026-08-09 10:57:40 +0200

    Update arg3.geo.js

--- arg3.geo.js
+++ arg3.geo.js
@@ -1,1 +1,17 @@
+// arg3.geo.js
+export function GEO_VECTOR_6D(gate, evo){
+  return {
+    x: gate.math.length * 10,
+    y: gate.home.length * 5,
+    z: gate.gate.length * 3,
+    w: evo.tick,
+    v: gate.boost,
+    u: (gate.boost % 9) * 7,
+    sat: gate.gate
+  };
+}
+
+export function GEO_MASSE(geo){
+  return geo.x + geo.y + geo.z + geo.v + geo.u;
+}
 
